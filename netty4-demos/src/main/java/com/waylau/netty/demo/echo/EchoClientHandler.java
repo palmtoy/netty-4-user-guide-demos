204069d73cd38fcf1bf5e10604c6b203a4f33526
lzg <lzg@pwrd.com>
2017-03-21 14:31:15 +0800

    * use another implementation for echo-cli

--- netty4-demos/src/main/java/com/waylau/netty/demo/echo/EchoClientHandler.java
+++ netty4-demos/src/main/java/com/waylau/netty/demo/echo/EchoClientHandler.java
@@ -1,29 +1,47 @@
 package com.waylau.netty.demo.echo;
 
-
 import java.util.Date;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
 
-@ChannelHandler.Sharable
-public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
-  @Override
-  public void channelActive(ChannelHandlerContext ctx) {
-    ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!\n", CharsetUtil.UTF_8));
-  }
+public class EchoClientHandler extends ChannelInboundHandlerAdapter {
 
-  @Override
-  public void channelRead0(ChannelHandlerContext ctx, ByteBuf in) {
-    Date d = new Date();
-    System.out.println(d + " ~ Client received : \'" + in.toString(CharsetUtil.UTF_8) + "\'");
-    System.exit(0);
-  }
+	private final ByteBuf firstMessage;
 
-  @Override
+	public EchoClientHandler() {
+		System.out.println("EchoClient.SIZE = " + EchoClient.SIZE);
+		firstMessage = Unpooled.buffer(EchoClient.SIZE+1);
+		for (int i = 1; i < firstMessage.capacity(); i ++) {
+			byte b = (byte)(i+64);
+			firstMessage.writeByte(b);
+		}
+		firstMessage.writeByte('\n');
+	}
+
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) {
+		ctx.writeAndFlush(firstMessage);
+	}
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) {
+		Date d = new Date();
+		ByteBuf in = (ByteBuf) msg;
+		System.out.println(d + " ~ Client received : \'" + in.toString(CharsetUtil.UTF_8) + "\'");
+	}
+
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) {
+		Date d = new Date();
+		System.out.println(d + " ~ ChannelReadComplete is running ... ");
+		ctx.flush();
+		System.exit(0);
+	}
+
+	@Override
   public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
     cause.printStackTrace();
     ctx.close();
